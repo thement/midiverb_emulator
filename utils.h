7bb6ae746d4cfc7f6ca561ef45f5924d5d4e4987
Jakub Horak <jakub.horak@ibawizard.net>
2025-12-28 10:57:53 +0100

    utils: Add function to read a block of data

--- utils.h
+++ utils.h
@@ -1,4 +1,5 @@
-// Error handling functions
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 static inline void pexit(const char *msg) {
     perror(msg);
     exit(EXIT_FAILURE);
@@ -13,4 +14,16 @@ static inline void die(const char *fmt, ...) {
     exit(EXIT_FAILURE);
 }
 
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+static inline void read_bytes(const char *path, int offset, int num_bytes, void *dest) {
+    FILE *f = fopen(path, "rb");
+    if (!f)
+        die("Failed to open file '%s': %s", path, strerror(errno));
+
+    if (fseek(f, offset, SEEK_SET) != 0)
+        die("Failed to seek to 0x%x in '%s': %s", offset, path, strerror(errno));
+
+    if (fread(dest, 1, num_bytes, f) != (size_t)num_bytes)
+        die("Failed to read from file: %s", strerror(errno));
+
+    fclose(f);
+}
